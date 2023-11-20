090bc3c79d74b6f829dd2f1bfd620f94befac645
LocTM <tranminhloc.anhk@gmail.com>
2023-11-20 21:58:56 +0700

    BT3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 // console.log("Số lượng số nguyên lớn hơn hoặc bằng 10 là: " + count);
 
 // BT2
-// const integerArray = [5, 12, 8, 25, 3, 17, 10, 15, 7, 20];
+// let integerArray = [5, 12, 8, 25, 3, 17, 10, 15, 7, 20];
 // let maxElement = 0;
 // let maxElementIndex = 0;
 //
@@ -22,3 +22,26 @@
 //
 // console.log("Giá trị lớn nhất trong mảng là: " + maxElement);
 // console.log("Vị trí của phần tử lớn nhất là: " + maxElementIndex);
+
+// BT2
+// let integerArray = [5, 12, 8, 25, 3, 17, 10, 15, 7, 20];
+// let maxElement = 0;
+// let maxElementIndex = 0;
+//
+// for (let i = 1; i < integerArray.length; i++) {
+//     if (integerArray[i] > maxElement) {
+//         maxElement = integerArray[i];
+//         maxElementIndex = i;
+//     }
+// }
+//
+// console.log("Giá trị lớn nhất trong mảng là: " + maxElement);
+// console.log("Vị trí của phần tử lớn nhất là: " + maxElementIndex);
+
+// BT3
+// let integerArray = [5, 12, 8, 25, 3, 17, 10, 15, 7, 20];
+// let maxElement = Math.max(...integerArray);
+// alert("Số lớn nhất trong mảng là: " + maxElement)
+// let sum = integerArray.reduce((acc, num) => acc + num, 0);
+// let average = sum / integerArray.length;
+// alert("Giá trị trung bình của các phần tử trong mảng là: " + average);
