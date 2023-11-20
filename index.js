8664f721bb3a092de42437a0e2f2401e7b91b0d7
LocTM <tranminhloc.anhk@gmail.com>
2023-11-20 21:42:51 +0700

    BT1

---
+++ index.js
@@ -0,0 +1,9 @@
+// // BT1
+// let arr = [1, 4, 14, 15, 18, 20, 22, 2, 3, 8];
+// let count = 0;
+// for (let i = 0; i < 10; i++) {
+//     if (arr[i] >= 10) {
+//         count++;
+//     }
+// }
+// console.log("Số lượng số nguyên lớn hơn hoặc bằng 10 là: " + count);
